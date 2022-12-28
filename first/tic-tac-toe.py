3cf10fbaa6760b5544e6f9a4c61718e4f12c0f01
RWall-MP <my.post.005.rewan@gmail.com>
2022-12-28 21:52:02 +0500

    Убейте меня, я сдаюсь. Помимо простого отображения ходов, я пытался добавить условия победы по резульатам которых игра прерывалась или продолжалась.

--- first/tic-tac-toe.py
+++ first/tic-tac-toe.py
@@ -10,97 +10,94 @@ field = [
 
 
 def check():
+    a = None
+    for i in range(4):
+        for j in field[i]:
+            if j == '-':
+                a = True
+                break
+
     if field[1][1] == 'x' and field[1][2] == 'x' and field[1][3] == 'x':
-        print(f'{player1} побеждает!')
-        return 'player1win'
+        print('Игрок №1 победил!')
+        return False
     elif field[2][1] == 'x' and field[2][2] == 'x' and field[2][3] == 'x':
-        print(f'{player1} побеждает!')
-        return 'player1win'
+        print('Игрок №1 победил!')
+        return False
     elif field[3][1] == 'x' and field[3][2] == 'x' and field[3][3] == 'x':
-        print(f'{player1} побеждает!')
-        return 'player1win'
-
+        print('Игрок №1 победил!')
+        return False
     elif field[1][1] == 'x' and field[2][1] == 'x' and field[3][1] == 'x':
-        print(f'{player1} побеждает!')
-        return 'player1win'
+        print('Игрок №1 победил!')
+        return False
     elif field[1][2] == 'x' and field[2][2] == 'x' and field[3][2] == 'x':
-        print(f'{player1} побеждает!')
-        return 'player1win'
+        print('Игрок №1 победил!')
+        return False
     elif field[1][3] == 'x' and field[2][3] == 'x' and field[3][3] == 'x':
-        print(f'{player1} побеждает!')
-        return 'player1win'
-
+        print('Игрок №1 победил!')
+        return False
     elif field[1][1] == 'x' and field[2][2] == 'x' and field[3][3] == 'x':
-        print(f'{player1} побеждает!')
-        return 'player1win'
-
-
+        print('Игрок №1 победил!')
+        return False
     elif field[1][1] == 'o' and field[1][2] == 'o' and field[1][3] == 'o':
-        print(f'{player2} побеждает!')
-        return 'player2win'
+        print('Игрок №2 победил!')
+        return False
     elif field[2][1] == 'o' and field[2][2] == 'o' and field[2][3] == 'o':
-        print(f'{player2} побеждает!')
-        return 'player2win'
+        print('Игрок №2 победил!')
+        return False
     elif field[3][1] == 'o' and field[3][2] == 'o' and field[3][3] == 'o':
-        print(f'{player2} побеждает!')
-        return 'player2win'
-
+        print('Игрок №2 победил!')
+        return False
     elif field[1][1] == 'o' and field[2][1] == 'o' and field[3][1] == 'o':
-        print(f'{player2} побеждает!')
-        return 'player2win'
+        print('Игрок №2 победил!')
+        return False
     elif field[1][2] == 'o' and field[2][2] == 'o' and field[3][2] == 'o':
-        print(f'{player2} побеждает!')
-        return 'player2win'
+        print('Игрок №2 победил!')
+        return False
     elif field[1][3] == 'o' and field[2][3] == 'o' and field[3][3] == 'o':
-        print(f'{player2} побеждает!')
-        return 'player2win'
-
+        print('Игрок №2 победил!')
+        return False
     elif field[1][1] == 'o' and field[2][2] == 'o' and field[3][3] == 'o':
-        print(f'{player2} побеждает!')
-        return 'player2win'
-
-    elif field != '-':
-        print('Ходы закончились. Ничья!')
-        return 'draw'
-
+        print('Игрок №2 победил!')
+        return False
+    elif a == False:
+        print('Ходы закончились. Игра окончена.')
+        return False
+    else:
+        return True
 
 def show_field():
     for row in field:
         for element in row:
             print(element, end=' ')
         print()
-    print(f'Для назначения своего хода используйте координаты \n'
-          f'сначала сверху (1, 2, 3), затем сбоку (1, 2, 3).')
+    #print(f'Для назначения своего хода используйте координаты \n'
+          #f'сначала сверху (1, 2, 3), затем сбоку (1, 2, 3).')
 
 
-def players_turn():
-    def player1_turn():
-        player1_chose = input(f'Сейчас ходит {player1}: ').split()
-        b1, a1 = int(player1_chose[0]), int(player1_chose[1])
-        if field[a1][b1] == '-':
-            field[a1][b1] = 'x'
-        else:
-            print('Эта клетка уже занята.')
-            player1_turn()
+def player1_turn():
+    player1_chose = input(f'Сейчас ходит {player1}: ').split()
+    b1, a1 = int(player1_chose[0]), int(player1_chose[1])
+    if field[a1][b1] == '-':
+        field[a1][b1] = 'x'
+    else:
+        print('Эта клетка уже занята.')
+        player1_turn()
 
+
+def player2_turn():
+    player2_chose = input(f'Сейчас ходит {player2}: ').split()
+    b2, a2 = int(player2_chose[0]), int(player2_chose[1])
+    if field[a2][b2] == '-':
+        field[a2][b2] = 'o'
+    else:
+        print('Эта клетка уже занята.')
+        player2_turn()
+
+
+while check():
     show_field()
-
-    def player2_turn():
-        player2_chose = input(f'Сейчас ходит {player2}: ').split()
-        b2, a2 = int(player2_chose[0]), int(player2_chose[1])
-        if field[a2][b2] == '-':
-            field[a2][b2] = 'o'
-        else:
-            print('Эта клетка уже занята.')
-            player2_turn()
-
     player1_turn()
-    show_field()
     check()
+    show_field()
     player2_turn()
-    show_field()
     check()
-
-
-players_turn()
-show_field()
